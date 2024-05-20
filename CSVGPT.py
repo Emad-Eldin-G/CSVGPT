f4156f3917d854ff01d63d14006ba05544a6aafe
Emad Gasser <omda.g@outlook.com>
2024-05-20 21:46:29 +0100

    reorganized everything

--- CSVGPT.py
+++ CSVGPT.py
@@ -9,110 +9,123 @@ import numpy as np
 from time import sleep
 from PIL import Image
 
-st.set_page_config(page_title="CSVGPT", page_icon="📊🪄", layout="wide", initial_sidebar_state="auto")
 
-#css styling
-st.markdown(
-    """
-    <style>
-    </style>
-    """
-    ,
-    unsafe_allow_html=True
-)
+def configure_page():
+    st.set_page_config(page_title="CSVGPT", page_icon="📊🪄", layout="wide", initial_sidebar_state="auto")
 
-
-#Sidebar
-st.sidebar.markdown("# CSVGPT | Open-Source 📊")
-st.sidebar.markdown("## Created by [EmadEldin Osman](https://github.com/Emad-Eldin-G)")
-st.sidebar.markdown("## To contribute, raise an issue on the [GitHub repo](https://github.com/Emad-Eldin-G/CSVGPT/issues)")
-st.sidebar.divider()
-st.sidebar.markdown("""
-### Powered by:  
-- [Streamlit](https://streamlit.io/)
-- [Pandas](https://pandas.pydata.org/)
-- [NumPy](https://numpy.org/)  
-- [Pandasai](https://pandas-ai.com/)
-""")
-st.sidebar.markdown("<br>", unsafe_allow_html=True)
-#Put images horizontally in the sidebar in a row in a container
-
-#Header
-title = st.title("CSVGPT")
-header = st.subheader("Blaze through datasets like a pro 🚀:H")
-
-#Help section
-open_button = st.toggle("How to use")
-
-if open_button:
+    # CSS styling
     st.markdown(
-    """
-    > In 3 simple steps
+        """
+        <style>
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
-    1. **Upload your dataset** in a CSV format
-    2. **Click Analyze** - Click the Analyze button to start analysis using our **AI**
-    3. **Get insights** - Get insights, graphs, and summaries on your dataset in seconds ⚡️
-        
+
+def configure_sidebar():
+    st.sidebar.markdown("# CSVGPT | Open-Source 📊")
+    st.sidebar.markdown("## Created by [EmadEldin Osman](https://github.com/Emad-Eldin-G)")
+    st.sidebar.markdown("## To contribute, raise an issue on the [GitHub repo](https://github.com/Emad-Eldin-G/CSVGPT/issues)")
+    st.sidebar.divider()
+    st.sidebar.markdown("""
+    ### Powered by:  
+    - [Streamlit](https://streamlit.io/)
+    - [Pandas](https://pandas.pydata.org/)
+    - [NumPy](https://numpy.org/)  
+    - [Pandasai](https://pandas-ai.com/)
     """)
-
-st.divider()
-#Free gap
-st.markdown("<br>", unsafe_allow_html=True)
-
-#Upload dataset
-st.markdown("## Upload your dataset :file_folder:")
-uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
-is_uploaded = False
-if uploaded_file is not None:
-    sleep(0.3)
-    st.success("Dataset uploaded successfully")
-    is_uploaded = True
+    st.sidebar.markdown("<br>", unsafe_allow_html=True)
 
 
-#Analyze button
-if is_uploaded:
-    analyze_button = st.button("Analyze 🔮", disabled=False)
-    try:
-        read_csv = pd.read_csv(uploaded_file)
-        csvgpt_instance = csvgpt(read_csv)
-    except Exception as e:
-        st.warning("Please upload a valid csv dataset")
-        analyze_button = st.button("Analyze 🔮", disabled=True)
-else:
-    analyze_button = st.button("Analyze 🔮", disabled=True)
+def display_header():
+    st.title("CSVGPT")
+    st.subheader("Blaze through datasets like a pro 🚀:H")
 
-#Used by analyzer.py to display responses from the analysis
-analyze_button_response = st.container()
 
-if analyze_button:
+def display_help_section():
+    open_button = st.toggle("How to use")
+    if open_button:
+        st.markdown(
+            """
+            > In 3 simple steps
+
+            1. **Upload your dataset** in a CSV format
+            2. **Click Analyze** - Click the Analyze button to start analysis using our **AI**
+            3. **Get insights** - Get insights, graphs, and summaries on your dataset in seconds ⚡️
+            """)
+
+
+def upload_dataset():
+    st.markdown("## Upload your dataset :file_folder:")
+    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+    is_uploaded = False
+    if uploaded_file is not None:
+        sleep(0.3)
+        st.success("Dataset uploaded successfully")
+        is_uploaded = True
+    return uploaded_file, is_uploaded
+
+
+def analyze_dataset(is_uploaded, uploaded_file):
     if is_uploaded:
-        #Analyze dataset
+        analyze_button = st.button("Analyze 🔮", disabled=False)
+        try:
+            read_csv = pd.read_csv(uploaded_file)
+            csvgpt_instance = csvgpt(read_csv)
+        except Exception as e:
+            st.warning("Please upload a valid csv dataset")
+            analyze_button = st.button("Analyze 🔮", disabled=True)
+    else:
+        analyze_button = st.button("Analyze 🔮", disabled=True)
+        csvgpt_instance = None
+
+    if analyze_button and is_uploaded:
         with st.spinner('Analyzing dataset... 🕵️‍♂️'):
             sleep(5)
         with st.spinner('Doing our magic... ✨'):
             sleep(5)
         with st.spinner('This might take a while... 🕰️'):
-            #Instantiate the csvgpt class
             csvgpt_instance.analyze()
+
+    return csvgpt_instance
+
+
+def ask_question(is_uploaded, csvgpt_instance):
+    if is_uploaded:
+        text_area = st.text_area("Ask CSVGPT a question about your data ✨", disabled=False)
+        submit_button = st.button("Ask CSVGPT", disabled=False)
     else:
-        #do nothing, the warning message will be sent from the catch error above
-        pass
+        text_area = st.text_area("Ask CSVGPT a question about your data ✨", disabled=True)
+        submit_button = st.button("Ask CSVGPT", disabled=True)
 
-st.markdown("<br>", unsafe_allow_html=True)
-st.markdown("<br>", unsafe_allow_html=True)
+    if submit_button and is_uploaded:
+        with st.spinner('Let us see... 🕵️‍♂️'):
+            sleep(2)
+            response = csvgpt_instance.ask(text_area)
+            st.write(response)
 
-#Ask questions about data (Uses Vana and LLMS)
-if is_uploaded:
-    text_area = st.text_area("Ask CSVGPT a question about your data ✨", disabled=False)
-    submit_button = st.button("Ask CSVGPT", disabled=False)
-else:
-    text_area = st.text_area("Ask CSVGPT a question about your data ✨", disabled=True)
-    submit_button = st.button("Ask CSVGPT", disabled=True)
 
-#Used by analyzer.py to display responses from the chatbot
-chat_response = st.container()
+def main():
+    configure_page()
+    configure_sidebar()
+    display_header()
+    display_help_section()
 
-if submit_button:
-    with st.spinner('Let us see... 🕵️‍♂️'):
-        sleep(2)
-        reponse = csvgpt_instance.ask(text_area)
+    st.divider()
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    uploaded_file, is_uploaded = upload_dataset()
+    csvgpt_instance = analyze_dataset(is_uploaded, uploaded_file)
+
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    ask_question(is_uploaded, csvgpt_instance)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        st.empty()
--- analyzer.py
+++ analyzer.py
@@ -7,8 +7,6 @@ from pandasai import Agent, SmartDataframe
 from pandasai.llm import OpenAI
 from pandasai.exceptions import NoCodeFoundError
 
-from container_manager import print_to_analyze_button_response, print_to_chat_response
-
 #This class manages the analysis of the dataset
 #It uses the pandas library to read the dataset and analyze it
 #It also uses the snowflake arctic LLM model to generate insights, and know what parts
@@ -17,33 +15,23 @@ from container_manager import print_to_analyze_button_response, print_to_chat_re
 
 class csvgpt:
     def __init__(self, dataset) -> None:
-        os.environ["PANDASAI_API_KEY"] = os.environ.get("PANDASAI_API_KEY")
-        self.llm = OpenAI(api_token=os.environ.get("OPENAI_API_KEY"))
-        self.analyze_manager = print_to_analyze_button_response()
-        self.chat_manager = print_to_chat_response()
         self.__dataset = dataset
         self.__back_slash = "\\"
 
-    @staticmethod
-    def print_reponse_in_yield_delay(response, delay=0.15):
-        for i in response:
-            yield i
-            sleep(delay)
 
     def analyze(self):
-        analyze_manager = print_to_analyze_button_response()
         self.__shape_of_dataset()
         self.__LLM_Analysis()
 
 
     def __shape_of_dataset(self):
-        self.analyze_manager.markdown("> Head of data")
-        self.analyze_manager.write(self.__dataset.head())
+        st.markdown("> Head of data")
+        st.write(self.__dataset.head())
 
-        self.analyze_manager.markdown("> Tail of data")
-        self.analyze_manager.write(self.__dataset.tail())
+        st.markdown("> Tail of data")
+        st.write(self.__dataset.tail())
 
-        self.analyze_manager.markdown("> Shape of data")
+        st.markdown("> Shape of data")
         pd_data_describe = self.__dataset.describe()
         data_row_count = len(self.__dataset)
         data_column_count = len(self.__dataset.columns)
@@ -51,7 +39,7 @@ class csvgpt:
         number_of_not_missing_values = self.__dataset.notnull().sum().sum()
         percentage_of_missing_values = (number_of_missing_values / (number_of_missing_values + number_of_not_missing_values)) * 100
 
-        self.analyze_manager.markdown(f"""
+        st.markdown(f"""
             - There are **{data_row_count}** rows in your dataset
             - There are **{data_column_count}** columns in your dataset
             - **{round(percentage_of_missing_values, 2)}%** of the cell values are missing ({number_of_missing_values} missing values)
@@ -60,14 +48,17 @@ class csvgpt:
 
 
     def __LLM_Analysis(self):
+        os.environ["PANDASAI_API_KEY"] = os.environ.get("PANDASAI_API_KEY")
+        llm = OpenAI(api_token=os.environ.get("OPENAI_API_KEY"))
+
         df = pd.DataFrame(self.__dataset)
-        pAI = SmartDataframe(df, config={"verbose": True, "llm": self.llm})
+        pAI = SmartDataframe(df, config={"verbose": True, "llm": llm})
 
         llm_analysis_response = pAI.chat("What is the context of this dataset, and what is it trying to find/track based on the context of the dataset? Also format your responsee by adding ** before and after key pieces of information")
-        self.analyze_manager.markdown(f"### Short summary of the dataset:")
-        self.analyze_manager.markdown(f"{llm_analysis_response}")
+        st.markdown(f"### Short summary of the dataset:")
+        st.markdown(f"{llm_analysis_response}")
         
-        self.analyze_manager.markdown("""
+        st.markdown("""
                     > Statistical Analysis of the dataset  
                     > Includes: min, max, mean, std, count, and more
         """)
@@ -81,26 +72,26 @@ class csvgpt:
         llm_analysis_response = pd.concat(pd_df, axis=1)
 
         try:
-            self.analyze_manager.table(llm_analysis_response)
+            st.table(llm_analysis_response)
         except StreamlitAPIException as e:
             #Raise error to sentry to see what what type of data caused the error
             pass
             try:
-                self.analyze_manager.write(llm_analysis_response)
+                st.write(llm_analysis_response)
             except NoCodeFoundError as e:
-                self.analyze_manager.markdown(llm_analysis_response)
+                st.markdown(llm_analysis_response)
         except NoCodeFoundError as e:
-            self.analyze_manager.markdown(llm_analysis_response)
+            st.markdown(llm_analysis_response)
 
     
     def ask(self, question):
-        chat_manager = print_to_chat_response()
+        os.environ["PANDASAI_API_KEY"] = os.environ.get("PANDASAI_API_KEY")
+        llm = OpenAI(api_token=os.environ.get("OPENAI_API_KEY"))
+
         df = pd.DataFrame(self.__dataset)
-        pAI = SmartDataframe(df, config={"verbose": True, "llm": self.llm})
+        pAI = SmartDataframe(df, config={"verbose": True, "llm": llm})
 
         pAI.chat("I want to ask a question about the dataset")
         llm_analysis_response = pAI.chat(question)
-        
-        with chat_manager.chat_message("Assistant"):
-            chat_manager.write(self.print_reponse_in_yield_delay(llm_analysis_response))
+        return llm_analysis_response
 
